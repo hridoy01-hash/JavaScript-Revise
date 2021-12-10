8c4d39dc234569517f43b8b0b81a2b9d4e150985
hridoy01-hash <86186273+hridoy01-hash@users.noreply.github.com>
2021-12-10 12:36:49 +0600

    operator

---
+++ operator.js
@@ -0,0 +1,12 @@
+let name1 = 'hridoy';
+let name2 = 'hasan';
+const fullName = name1 + name2;
+console.log(fullName);
+
+const number1 = 45 
+
+const result = number1.toString(3);
+console.log(result);
+
+
+
